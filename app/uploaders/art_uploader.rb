a0aa4639ae9f7012c40c71c7f929ff8c0745c798
Jeremy Smith <jmsmith57@hotmail.com>
2016-08-07 12:21:32 +0100

    users can upload images and edit them, also rmagick is being used to display the pictures

--- app/uploaders/art_uploader.rb
+++ app/uploaders/art_uploader.rb
@@ -3,7 +3,7 @@
 class ArtUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -32,10 +32,12 @@ class ArtUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
+  version :thumb do
+    process :resize_to_fit => [400, 400]
+  end
+  version :detail do
+    process :resize_to_fit => [1000, 1000]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
