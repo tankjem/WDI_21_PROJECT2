9875ea3853a094cb0c300ffc01ce4866f5f481f6
Jeremy Smith <jmsmith57@hotmail.com>
2016-08-09 11:03:58 +0100

    cards are now lined up next to each other

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :genres 
   has_and_belongs_to_many :platforms
   mount_uploader :image, ArtUploader
+
+  # scope :genres, -> (genres) { where genres: name }
+  # scope :platformd, -> (platforms) { where platforms: name }
 end
