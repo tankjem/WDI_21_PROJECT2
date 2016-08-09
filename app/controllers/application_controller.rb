f84dfd8fc02c2debff650478b6ee7b7116776aba
Jeremy Smith <jmsmith57@hotmail.com>
2016-08-09 19:16:48 +0100

    fixed the edit submit to route back to user show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,4 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource)
       root_path
     end
-    def after_update_path_for(resource)
-      user_path(current_user)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   resources :posts, :comments
-  devise_for :users
+  devise_for :users, controllers: { registrations: :registrations }
   root "statics#index"
   get 'about', to: 'statics#about'
-  get '/users/:id', to: 'users#show', as: "user" 
+  get '/users/:id', to: 'users#show', as: "user"
+
 end
