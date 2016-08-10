54e9a77cd80ccf858d4feca039efdf9779a117b8
Jeremy Smith <jmsmith57@hotmail.com>
2016-08-10 10:06:34 +0100

    ironed out a few security issus and seeded comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :check_user_own_post, only:[:edit, :update, :destroy]
   # GET /posts
   # GET /posts.json
   def index
@@ -74,6 +74,11 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+    end
+    def check_user_own_post
+      if @post.user != current_user
+        redirect_to posts_path, notice: "uh uh uh uh you didn't say the magic word"
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,13 @@ require_relative '../app/models/genre.rb'
 require_relative '../app/models/post.rb'
 require_relative '../app/models/platform.rb'
 require_relative '../app/models/user.rb'
+require_relative '../app/models/comment.rb'
 
 genre = Genre.destroy_all
 platform = Platform.destroy_all
 user = User.destroy_all
 post = Post.destroy_all
+comment = Comment.destroy_all
 
 genre1 = Genre.create(name:"Action")
 genre2 = Genre.create(name:"Adventure")
@@ -129,44 +131,84 @@ post18 = Post.create(game:"Jet Set Radio Future",
   description:"JSRF Grafitti Art",
   image:File.open(File.join(Rails.root,'/public/uploads/post/image/18/XM8UE4o.jpg')))
 
+comment1 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment2 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment3 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment4 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment5 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment6 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment7 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment8 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment9 = Comment.create(title:"Testing Testing", content:" Your simply the best!")
+comment10 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment11 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment12 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment13 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment14 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment15 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment16 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment17 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment18 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment19 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+comment20 = Comment.create(title:"Testing Testing", content:"No Your the best!")
+
 post1.genres = [genre17]
 post1.platforms = [platform3]
+post1.comments = [comment1, comment11]
 post2.genres = [genre1, genre2, genre4, genre5, genre7]
 post2.platforms =[platform1, platform2, platform4, platform5, platform6, platform7]
+post2.comments = [comment2, comment12]
 post3.genres = [genre1, genre2, genre3]
 post3.platforms = [platform6, platform7]
+post3.comments = [comment3]
 post4.genres = [genre2]
 post4.platforms =[platform3, platform8, platform9]
+post4.comments = [comment20]
 post5.genres = [genre5, genre6, genre7]
 post5.platforms = [platform1, platform7]
+post5.comments = [comment4]
 post6.genres = [genre18]
 post6.platforms = [platform9]
+post6.comments = [comment5]
 post7.genres = [genre1, genre2, genre4, genre5]
 post7.platforms = [platform1, platform2, platform4, platform5, platform6, platform7]
+post7.comments = [comment6]
 post8.genres = [genre2, genre17]
 post8.platforms = [platform3, platform8, platform9]
+post8.comments = [comment7]
 post9.genres = [genre1, genre2, genre12, genre14]
 post9.platforms = [platform6, platform7]
+post9.comments = [comment7]
 post10.genres = [genre1, genre2, genre3]
 post10.platforms = [platform6, platform7]
+post10.comments = [comment8]
 post11.genres = [genre2, genre3, genre12]
 post11.platforms = [platform6, platform7]
+post11.comments = [comment9]
 post12.genres = [genre4, genre5, genre7]
 post12.platforms = [platform1, platform4, platform5, platform6, platform7]
+post12.comments = [comment13]
 post13.genres = [genre2, genre12]
 post13.platforms = [platform3, platform8, platform9]
+post13.comments = [comment14]
 post14.genres = [genre1, genre2, genre5, genre6]
 post14.platforms = [platform1, platform7]
+post14.comments = [comment15]
 post15.genres = [genre12]
 post15.platforms = [platform6, platform9]
+post15.comments = [comment16]
 post16.genres = [genre1, genre2, genre17]
 post16.platforms = [platform6, platform7]
+post16.comments = [comment17]
 post17.genres = [genre2, genre12]
 post17.platforms = [platform3, platform8, platform9]
+post17.comments = [comment18]
 post18.genres = [genre2, genre5, genre8, genre9]
 post18.platforms = [platform1, platform2, platform4, platform5, platform6, platform7]
+post18.comments = [comment19]
 
-
+user1.comments = [comment1, comment2, comment3, comment4, comment5, comment6, comment7, comment8, comment9, comment10]
+user2.comments = [comment11, comment12, comment13, comment14, comment15, comment16, comment17, comment18, comment19, comment20]
 
 
 
